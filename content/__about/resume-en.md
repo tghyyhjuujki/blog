5a395742aa28c062c28448d46773d43ff4ee6719
정훈 송 <tghyyhjuujki@naver.com>
2020-08-29 21:03:33 +0900

    update

---
+++ content/__about/resume-en.md
@@ -0,0 +1,29 @@
+---
+title: 'about'
+date: 2020-7-03 16:21:13
+lang: 'en'
+---
+
+# Jeonghoon Song
+
+<!-- <div align="center"> </div>-->
+
+<br/>
+
+## Profile
+
+어제보다 나은 오늘을 위해!
+
+GitHub : https://github.com/tghyyhjuujki
+
+Email : tghyyhju@gmail.com
+
+<br/>
+
+## Education
+
+2012.03 ~ 2019.02 | 상명대학교 게임학과
+
+2019.12 ~ 2020.06 | 삼성 멀티캠퍼스 Cloud MSA
+
+2020.07 ~ Present | SK Infosec Cloud Security
---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `JohnLemon`,
+  description: `Blog posted about ...`,
+  author: `Jeonghoon Song`,
+  introduction: 'DevOps를 꿈꾸는 개발자입니다!',
+  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `tghyyhjuujki`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'tghyyhjuujki/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: 'UA-176748195-1', // Add your google analytics tranking ID
+}
